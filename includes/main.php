5506d18ed1df253b03a1ddb21148ea43ee9f2c23
FYNN TERVOORT <40134@ma-web.nl>
2026-05-21 12:04:21 +0200

    api add voor actuele temperatuur

--- includes/main.php
+++ includes/main.php
@@ -1,8 +1,18 @@
+<?php
+$temperatuur = null;
+$apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.3676&longitude=4.9041&current=temperature_2m&timezone=Europe/Amsterdam";
+$response = @file_get_contents($apiUrl);
+if ($response !== false) {
+    $data = json_decode($response, true);
+    if (isset($data['current']['temperature_2m'])) {
+        $temperatuur = $data['current']['temperature_2m'];
+    }
+}
+?>
 <main class="main-page">
     <section class="dashboard">
         <div class="block-row">
             <article class="dashboard-block">
-
             </article>
             <article class="dashboard-block">
 
@@ -11,6 +21,10 @@
         <div class="block-row">
             <div class="block-grid">
                 <article class="dashboard-grid-block">
+                <h1>Actuele temperatuur Amsterdam</h1>
+                <p class="temperatuur">
+                    <?php echo $temperatuur !== null ? htmlspecialchars($temperatuur) . " &deg;C" : "Niet beschikbaar"; ?>
+                </p>
 
                 </article>
                 <article class="dashboard-grid-block">
