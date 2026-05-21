2585e9bcb7d16e4d621a5b9358069f8fe5008987
Nathan <nathanjuliannj@gmail.com>
2026-05-21 10:55:58 +0200

    main done

---
+++ includes/main.php
@@ -0,0 +1,50 @@
+<main class="main-page">
+    <section class="dashboard">
+        <div class="block-row">
+            <article class="dashboard-block">
+
+            </article>
+            <article class="dashboard-block">
+
+            </article>
+        </div>
+        <div class="block-row">
+            <div class="block-grid">
+                <article class="dashboard-grid-block">
+
+                </article>
+                <article class="dashboard-grid-block">
+
+                </article>
+                <article class="dashboard-grid-block">
+
+                </article>
+                <article class="dashboard-grid-block">
+
+                </article>
+            </div>
+            <div class="block-grid">
+                <article class="dashboard-grid-block">
+
+                </article>
+                <article class="dashboard-grid-block">
+
+                </article>
+                <article class="dashboard-grid-block">
+
+                </article>
+                <article class="dashboard-grid-block">
+
+                </article>
+            </div>
+        </div>
+        <div class="block-row">
+            <article class="dashboard-block">
+
+            </article>
+            <article class="dashboard-block">
+
+            </article>
+        </div>
+    </section>
+</main>
