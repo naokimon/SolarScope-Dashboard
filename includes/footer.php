2ce529ec5d82870a40ac9dcd1224786a2d18c39d
FYNN TERVOORT <40134@ma-web.nl>
2026-05-21 10:24:53 +0200

    index, footer en style

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="style.css">
+
+<footer class="footer">
+	<h1> Nathan • Fynn • Petra • Zakaria </h1>
+	<p id="copyright"> ©SolarScope</p>
+</footer>
--- index.php
+++ index.php
@@ -5,8 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SolarScope - Dashboard</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/SolarScope.png" type="image/x-icon">
 </head>
 <body>
-    <?php include_once "includes/header.php"?>
+
+<?php include_once "includes/header.php"?>    
+
+
+<?php include_once 'includes/footer.php'; ?>
+    
 </body>
 </html>
